1db8a30d4c8ff39c592237341055328912c5a6cc
pencilCool <yhtang_xt@163.com>
2017-04-07 00:43:15 +0800

    add login UI

---
+++ UESTC/UESTC/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UESTC
+//
+//  Created by tangyuhua on 2017/4/6.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      self.tableView.backgroundColor = UIColor.yhWaterBlueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+
+
+}
+
---
+++ UESTC/UESTC/UIColor+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extension.swift
+//  UESTC
+//
+//  Created by tangyuhua on 2017/4/7.
+//  Copyright © 2017年 tangyuhua. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIColor {
+    
+    class func yhWaterBlueColor() -> UIColor {
+        // 61A8FF
+        return UIColor(red: 97/255.0, green: 168.0/255.0, blue: 1, alpha: 1.0)
+    }
+    
+    
+    
+    
+}
